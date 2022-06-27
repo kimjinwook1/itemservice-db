69729f3e8f70c0f53946e72e3c2410553258dfbd
kimjinwook1 <rlawlsdnr628@naver.com>
2022-06-27 20:23:45 +0900

    JdbcTemplate 적용 - 구성과 실행

--- src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
+++ src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
@@ -74,7 +74,6 @@ public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
 	public List<Item> findAll(ItemSearchCond cond) {
 		String itemName = cond.getItemName();
 		Integer maxPrice = cond.getMaxPrice();
-
 		String sql = "select id, item_name, price, quantity from item";
 		//동적 쿼리
 		if (StringUtils.hasText(itemName) || maxPrice != null) {
@@ -96,7 +95,7 @@ public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
 			param.add(maxPrice);
 		}
 		log.info("sql={}", sql);
-		return template.query(sql, itemRowMapper(), param);
+		return template.query(sql, itemRowMapper(), param.toArray());
 	}
 
 	private RowMapper<Item> itemRowMapper() {
